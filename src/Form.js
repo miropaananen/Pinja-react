366c67fc9a4888c199124ba3c66c03933ebad7c5
miropaananen <miro.mikael.paananen@gmail.com>
2023-01-03 19:42:53 +0200

    Change Listview layout, Add Delete button to Listview

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,4 @@
-
 import React, { useState } from 'react';
-//import { useState } from 'react';
 
 export default function Form() {
   const [data, setData] = useState([]);
@@ -11,19 +9,31 @@ export default function Form() {
     e.preventDefault();
 
     // Add the new data to the existing data array
-    setData([...data, {id, name: e.target.name.value, email: e.target.email.value }]);
+    setData([...data, {id, name: e.target.name.value, description: e.target.description.value, comment:e.target.comment.value }]);
 
     // Save the data array to local storage
     localStorage.setItem('data', JSON.stringify(data));
 
     // Clear the form
     e.target.name.value = '';
-    e.target.email.value = '';
+    e.target.description.value = '';
+    e.target.comment.value = '';
 
     //iterate id every time data is saved
     setId(id + 1);
 
+    console.log(e);
+
   };
+
+  const handleDelete = (id) => {
+    // Remove the item with the specified ID from the data array
+    setData(data.filter((item) => item.id !== id));
+
+    // Remove the item from local storage
+    localStorage.removeItem(id);
+  };
+
 
   //console.log(data);
   console.log("foreach Javascrip objekti");
@@ -38,16 +48,21 @@ export default function Form() {
       </label>
       <br />
       <label>
-        Email:
-        <input type="email" name="email" />
+        Description
+        <input type="text" name="description" />
+      </label>
+      <br />
+      <label>
+        Comment
+        <input type="text" name="comment" />
       </label>
       <br />
       <button type="submit">Save</button>
     </form>
 
-  <ul>
-    {data.map((item) => <li key={item.id}> {item.name} {item.email}</li>)}
-  </ul>
+  <div className='Listview'>
+    {data.map((item) => <div key={item.id}> {item.name} {item.description} <button onClick={() => handleDelete(item.id)}>Delete</button></div>)}
+  </div>
   </div>
 
   );
